1c4248a9439da9ccdb986a335afa6bcf1bf23064
NiBienvenu <nicibienvenu@gmail.com>
2025-06-12 08:04:36 +0200

    changement d'utlisateur

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'bienvenu@gmail.com',
+            'name' => 'Ideal Admin',
+            'email' => 'idealmanager@gmail.com',
             'password' => \Hash::make('12345678'),
         ]);
     }
